d8a64e593f74f1763a0604ecbef467978a3773d1
Akintayo A. Olusegun <akintayo.segun@gmail.com>
2021-09-23 14:57:20 +0400

    Show test networks toggle button in settings/advanced tab.
    Signed-off-by: Akintayo A. Olusegun <akintayo.segun@gmail.com>

--- ui/app/store/actions.js
+++ ui/app/store/actions.js
@@ -2076,6 +2076,10 @@ export function setShowFiatConversionOnTestnetsPreference(value) {
   return setPreference('showFiatInTestnets', value);
 }
 
+export function setShowTestNetworks(value) {
+  return setPreference('showTestNetworks', value);
+}
+
 export function setAutoLockTimeLimit(value) {
   return setPreference('autoLockTimeLimit', value);
 }
