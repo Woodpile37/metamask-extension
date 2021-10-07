409d59dd15437e961d46dfc78451d6c5ef3e2a59
George Marshall <georgewrmarshall@gmail.com>
2021-10-07 14:45:50 -0700

    Adding story for ToggleButton

--- ui/components/ui/toggle-button/toggle-button.stories.js
+++ ui/components/ui/toggle-button/toggle-button.stories.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { useArgs } from '@storybook/client-api';
 import README from './README.mdx';
+import React, { useState } from 'react';
+import { action } from '@storybook/addon-actions';
+import { boolean, text } from '@storybook/addon-knobs';
+
 import ToggleButton from './toggle-button.component';
 
 export default {
   title: 'Components/UI/ToggleButton',
+  title: 'ToggleButton',
   component: ToggleButton,
 
   parameters: {
@@ -19,14 +24,29 @@ export default {
     onLabel: { control: 'text' },
     disabled: { control: 'boolean' },
   },
+  id: __filename,
 };
 
 export const DefaultStory = (args) => {
   const [{ value }, updateArgs] = useArgs();
   const handleOnToggle = () => {
     updateArgs({ value: !value });
+export const DefaultStory = () => {
+  const [checked, setChecked] = useState(false);
+  const handleOnToggle = (e) => {
+    action('onToggle')(e);
+    setChecked(!checked);
   };
   return <ToggleButton {...args} value={value} onToggle={handleOnToggle} />;
+  return (
+    <ToggleButton
+      offLabel={text('offLabel', 'off')}
+      onLabel={text('onLabel', 'on')}
+      disabled={boolean('disabled', false)}
+      value={checked}
+      onToggle={handleOnToggle}
+    />
+  );
 };
 
 DefaultStory.storyName = 'Default';
@@ -35,4 +55,6 @@ DefaultStory.args = {
   offLabel: 'off',
   onLabel: 'on',
   disabled: false,
+DefaultStory.story = {
+  name: 'Default',
 };
