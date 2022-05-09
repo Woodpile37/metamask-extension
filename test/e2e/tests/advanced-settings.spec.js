7b8e037f04dacbb040383a2dd88f63415e32bdaa
seaona <mariona@gmx.es>
2022-05-09 12:10:25 +0200

    Complete testnet conversion for all testnets

--- test/e2e/tests/advanced-settings.spec.js
+++ test/e2e/tests/advanced-settings.spec.js
@@ -35,6 +35,7 @@ describe('Advanced Settings', function () {
           '.settings-page__header__title-container__close-button',
         );
 
+<<<<<<< HEAD
         const secondaryCurrency = await driver.findElement(
           '[data-testid="eth-overview__secondary-currency"] .currency-display-component__suffix',
         );
@@ -42,6 +43,57 @@ describe('Advanced Settings', function () {
           await secondaryCurrency.getText(),
           'USD',
           `Secondary currency is not present`,
+=======
+        // Ropsten
+        await driver.clickElement(
+          '.app-header__network-component-wrapper div',
+        );
+        await driver.clickElement(
+          {text: 'Ropsten Test Network', tag: 'span'}
+        )
+
+        let secondaryCurrency = await driver.isElementPresent(
+          '[data-testid="eth-overview__secondary-currency"]',
+        );
+        assert.equal(await secondaryCurrency, true);
+
+        // Kovan
+        await driver.clickElement(
+          '.app-header__network-component-wrapper div',
+        );
+        await driver.clickElement(
+          {text: 'Kovan Test Network', tag: 'span'}
+        )
+
+        secondaryCurrency = await driver.isElementPresent(
+          '[data-testid="eth-overview__secondary-currency"]',
+        );
+        assert.equal(await secondaryCurrency, true);
+
+        // Rinkeby
+        await driver.clickElement(
+          '.app-header__network-component-wrapper div',
+        );
+        await driver.clickElement(
+          {text: 'Rinkeby Test Network', tag: 'span'}
+        )
+
+        secondaryCurrency = await driver.isElementPresent(
+          '[data-testid="eth-overview__secondary-currency"]',
+        );
+        assert.equal(await secondaryCurrency, true);
+
+        // Goerli
+        await driver.clickElement(
+          '.app-header__network-component-wrapper div',
+        );
+        await driver.clickElement(
+          {text: 'Goerli Test Network', tag: 'span'}
+        )
+
+        secondaryCurrency = await driver.isElementPresent(
+          '[data-testid="eth-overview__secondary-currency"]',
+>>>>>>> efda2b9fa8 (Complete testnet conversion for all testnets)
         );
       },
     );
@@ -58,10 +110,73 @@ describe('Advanced Settings', function () {
       async ({ driver }) => {
         await unlockWallet(driver);
 
-        const secondaryCurrency = await driver.isElementPresent(
+        await driver.clickElement('.account-menu__icon');
+        await driver.clickElement({ text: 'Settings', tag: 'div' });
+        await driver.clickElement({ text: 'Advanced', tag: 'div' });
+
+        await driver.clickElement(
+          '[data-testid="advanced-setting-show-testnet-conversion"] label',
+        );
+        
+        await driver.clickElement(
+          '.settings-page__header__title-container__close-button',
+        );
+
+        // Ropsten
+        await driver.clickElement(
+          '.app-header__network-component-wrapper div',
+        );
+        await driver.clickElement(
+          {text: 'Ropsten Test Network', tag: 'span'}
+        )
+
+        let secondaryCurrency = await driver.isElementPresent(
           '[data-testid="eth-overview__secondary-currency"]',
         );
+<<<<<<< HEAD
         assert.equal(secondaryCurrency, false, `Secondary currency is present`);
+=======
+        assert.equal(await secondaryCurrency, false);
+
+        // Kovan
+        await driver.clickElement(
+          '.app-header__network-component-wrapper div',
+        );
+        await driver.clickElement(
+          {text: 'Kovan Test Network', tag: 'span'}
+        )
+
+        secondaryCurrency = await driver.isElementPresent(
+          '[data-testid="eth-overview__secondary-currency"]',
+        );
+        assert.equal(await secondaryCurrency, false);
+
+        // Rinkeby
+        await driver.clickElement(
+          '.app-header__network-component-wrapper div',
+        );
+        await driver.clickElement(
+          {text: 'Rinkeby Test Network', tag: 'span'}
+        )
+
+        secondaryCurrency = await driver.isElementPresent(
+          '[data-testid="eth-overview__secondary-currency"]',
+        );
+        assert.equal(await secondaryCurrency, false);
+
+        // Goerli
+        await driver.clickElement(
+          '.app-header__network-component-wrapper div',
+        );
+        await driver.clickElement(
+          {text: 'Goerli Test Network', tag: 'span'}
+        )
+
+        secondaryCurrency = await driver.isElementPresent(
+          '[data-testid="eth-overview__secondary-currency"]',
+        );
+        assert.equal(await secondaryCurrency, false);
+>>>>>>> efda2b9fa8 (Complete testnet conversion for all testnets)
       },
     );
   });
