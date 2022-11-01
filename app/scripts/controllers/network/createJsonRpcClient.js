d88cf7b0e2d825ad77b82cee40cd9d448ff5dc5f
Zachary Belford <belfordz66@gmail.com>
2022-10-31 17:05:50 -0700

    Add shared network tests to createJsonRPCClient
    Changes the original shared-tests & underlying helpers for network
    middleware testing. Also Removes test middleware from createJsonRpcClient

---
+++ app/scripts/controllers/network/createJsonRpcClient.js
@@ -0,0 +1,44 @@
+import { mergeMiddleware } from 'json-rpc-engine';
+import {
+  createFetchMiddleware,
+  createBlockRefRewriteMiddleware,
+  createBlockCacheMiddleware,
+  createInflightCacheMiddleware,
+  createBlockTrackerInspectorMiddleware,
+  providerFromMiddleware,
+} from 'eth-json-rpc-middleware';
+import { PollingBlockTracker } from 'eth-block-tracker';
+import { SECOND } from '../../../../shared/constants/time';
+
+const inTest = process.env.IN_TEST;
+const blockTrackerOpts = inTest ? { pollingInterval: SECOND } : {};
+
+export default function createJsonRpcClient({ rpcUrl, chainId }) {
+  const fetchMiddleware = createFetchMiddleware({ rpcUrl });
+  const blockProvider = providerFromMiddleware(fetchMiddleware);
+  const blockTracker = new PollingBlockTracker({
+    ...blockTrackerOpts,
+    provider: blockProvider,
+  });
+
+  const networkMiddleware = mergeMiddleware([
+    createChainIdMiddleware(chainId),
+    createBlockRefRewriteMiddleware({ blockTracker }),
+    createBlockCacheMiddleware({ blockTracker }),
+    createInflightCacheMiddleware(),
+    createBlockTrackerInspectorMiddleware({ blockTracker }),
+    fetchMiddleware,
+  ]);
+
+  return { networkMiddleware, blockTracker };
+}
+
+function createChainIdMiddleware(chainId) {
+  return (req, res, next, end) => {
+    if (req.method === 'eth_chainId') {
+      res.result = chainId;
+      return end();
+    }
+    return next();
+  };
+}
