99399de01167f3690d4cdb6d10f807d96d4f17ea
Woodpile37 <thawood37@hotmail.com>
2024-02-21 22:18:32 -0600

    fixup! fix: package.json to reduce vulnerabilities

--- ui/pages/confirmation/templates/add-ethereum-chain.test.js
+++ ui/pages/confirmation/templates/add-ethereum-chain.test.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import configureMockStore from 'redux-mock-store';
-import thunk from 'redux-thunk';
 import { waitFor } from '@testing-library/react';
-
-import { NetworkStatus } from '@metamask/network-controller';
 import { renderWithProvider } from '../../../../test/lib/render-helpers';
 import { MESSAGE_TYPE } from '../../../../shared/constants/app';
 
@@ -11,46 +8,6 @@ import Confirmation from '../confirmation';
 
 jest.mock('../../../../shared/lib/fetch-with-cache');
 
-const middleware = [thunk];
-
-const mockApprovalId = 1;
-const mockApproval = {
-  id: mockApprovalId,
-  origin: 'https://test-dapp.metamask.io',
-  requestData: {
-    rpcUrl: 'https://rpcurl.test.chain',
-    rpcPrefs: {
-      blockExplorerUrl: 'https://blockexplorer.test.chain',
-    },
-    chainName: 'Test chain',
-    ticker: 'TST',
-    chainId: '0x9999',
-    nickname: 'Test chain',
-  },
-};
-
-const mockBaseStore = {
-  metamask: {
-    pendingApprovals: {
-      [mockApprovalId]: mockApproval,
-    },
-    approvalFlows: [{ id: mockApprovalId, loadingText: null }],
-    subjectMetadata: {},
-    providerConfig: {
-      type: 'rpc',
-      rpcUrl: 'http://example-custom-rpc.metamask.io',
-      chainId: '0x9999',
-      nickname: 'Test initial state',
-    },
-    selectedNetworkClientId: 'test-network-client-id',
-    networksMetadata: {
-      'test-network-client-id': {
-        EIPS: {},
-        status: NetworkStatus.Available,
-      },
-    },
-  },
-};
 
 describe('add-ethereum-chain confirmation', () => {
   it('should match snapshot', async () => {
