035924f3ab32069e3e24fd2f6156f25d9125069d
Restyled.io <commits@restyled.io>
2024-02-22 09:50:26 +0000

    Restyled by clang-format

--- ui/pages/confirmation/templates/add-ethereum-chain.test.js
+++ ui/pages/confirmation/templates/add-ethereum-chain.test.js
@@ -1,36 +1,36 @@
+import {waitFor} from '@testing-library/react';
 import React from 'react';
 import configureMockStore from 'redux-mock-store';
-import { waitFor } from '@testing-library/react';
-import { renderWithProvider } from '../../../../test/lib/render-helpers';
-import { MESSAGE_TYPE } from '../../../../shared/constants/app';
 
+import {MESSAGE_TYPE} from '../../../../shared/constants/app';
+import {renderWithProvider} from '../../../../test/lib/render-helpers';
 import Confirmation from '../confirmation';
 
 jest.mock('../../../../shared/lib/fetch-with-cache');
 
-
 describe('add-ethereum-chain confirmation', () => {
   it('should match snapshot', async () => {
     const testStore = {
-      metamask: {
+      metamask : {
         ...mockBaseStore.metamask,
-        pendingApprovals: {
-          [mockApprovalId]: {
+        pendingApprovals : {
+          [mockApprovalId] : {
             ...mockApproval,
-            'type': MESSAGE_TYPE.ADD_ETHEREUM_CHAIN,
+            'type' : MESSAGE_TYPE.ADD_ETHEREUM_CHAIN,
           },
         },
       },
     };
     const store = configureMockStore(middleware)(testStore);
-    const { container, getByText } = renderWithProvider(
-      <Confirmation />,
-      store,
+    const {container, getByText} = renderWithProvider(
+        <Confirmation />,
+        store,
     );
     await waitFor(() => {
       expect(
-        getByText('MetaMask does not verify custom networks.'),
-      ).toBeInTheDocument();
+          getByText('MetaMask does not verify custom networks.'),
+          )
+          .toBeInTheDocument();
       expect(container.querySelector('.callout')).toBeDefined();
       expect(container).toMatchSnapshot();
     });
--- ui/pages/confirmation/templates/middleware.js
+++ ui/pages/confirmation/templates/middleware.js
@@ -1,35 +1,36 @@
-export const middleware = [thunk], mockApprovalId = 1, mockApproval = {
-  id: mockApprovalId,
-  origin: 'https://test-dapp.metamask.io',
-  requestData: {
-    rpcUrl: 'https://rpcurl.test.chain',
-    rpcPrefs: {
-      blockExplorerUrl: 'https://blockexplorer.test.chain',
+export const middleware = [ thunk ], mockApprovalId = 1, mockApproval = {
+  id : mockApprovalId,
+  origin : 'https://test-dapp.metamask.io',
+  requestData : {
+    rpcUrl : 'https://rpcurl.test.chain',
+    rpcPrefs : {
+      blockExplorerUrl : 'https://blockexplorer.test.chain',
     },
-    chainName: 'Test chain',
-    ticker: 'TST',
-    chainId: '0x9999',
-    nickname: 'Test chain',
+    chainName : 'Test chain',
+    ticker : 'TST',
+    chainId : '0x9999',
+    nickname : 'Test chain',
   },
-}, mockBaseStore = {
-  metamask: {
-    pendingApprovals: {
-      [mockApprovalId]: mockApproval,
-    },
-    approvalFlows: [{ id: mockApprovalId, loadingText: null }],
-    subjectMetadata: {},
-    providerConfig: {
-      type: 'rpc',
-      rpcUrl: 'https:///example-custom-rpc.metamask.io',
-      chainId: '0x9999',
-      nickname: 'Test initial state',
-    },
-    selectedNetworkClientId: 'test-network-client-id',
-    networksMetadata: {
-      'test-network-client-id': {
-        EIPS: {},
-        status: NetworkStatus.Available,
-      },
-    },
-  },
-};
+},
+             mockBaseStore = {
+               metamask : {
+                 pendingApprovals : {
+                   [mockApprovalId] : mockApproval,
+                 },
+                 approvalFlows : [ {id : mockApprovalId, loadingText : null} ],
+                 subjectMetadata : {},
+                 providerConfig : {
+                   type : 'rpc',
+                   rpcUrl : 'https:///example-custom-rpc.metamask.io',
+                   chainId : '0x9999',
+                   nickname : 'Test initial state',
+                 },
+                 selectedNetworkClientId : 'test-network-client-id',
+                 networksMetadata : {
+                   'test-network-client-id' : {
+                     EIPS : {},
+                     status : NetworkStatus.Available,
+                   },
+                 },
+               },
+             };
